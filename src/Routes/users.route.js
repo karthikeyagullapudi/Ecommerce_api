908fa1dcf7268c8f5fce209c71a56e67920cbd84
Prabhavathi-Godatha <prabhavathigodatha15@gmail.com>
2025-06-04 12:21:51 +0530

    allProducts

--- src/Routes/users.route.js
+++ src/Routes/users.route.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
-import addProduct from "../Controllers/user.controller.js";
+import { addProduct, getAllProducts } from "../Controllers/user.controller.js";
 import userValidator from "../Validators/users.validators.js";
 
 const user = Router();
 user.route("/addproduct").post(userValidator, addProduct);
+user.route("/getallproducts").get(getAllProducts);
 export default user;
