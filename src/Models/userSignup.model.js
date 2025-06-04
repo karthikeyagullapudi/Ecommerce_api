b52a9a028dd0e64a2af9c15050b7ae3fb25280af
karthikeyagullapudi <karthikgullapudi@gmail.com>
2025-06-04 15:23:51 +0530

    writen apis for usersignup

---
+++ src/Models/userSignup.model.js
@@ -0,0 +1,31 @@
+import { Schema } from "mongoose";
+import bcrypt from "bcrypt";
+
+const userSignup = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+userSignup.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  try {
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(this.password, saltRounds);
+    this.password = hashedPassword;
+    next();
+  } catch (error) {
+    console.error("Error hashing password:", error);
+    next(error);
+  }
+});
+
+export default model("userSignup", userSignup);
