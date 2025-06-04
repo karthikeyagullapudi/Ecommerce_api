19f061d9c99a976e07a73643ff74e7ebbfbb4060
Sagar3981 <sagar.pitta81@gmail.com>
2025-06-04 15:25:13 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import express from "express";
 import userRouter from "./Routes/users.route.js";
+import cors from "cors";
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use("/api", userRouter);
 export default app;
