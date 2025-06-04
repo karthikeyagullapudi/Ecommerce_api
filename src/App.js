6f3524c2bbdcf5bc75180e6b2e1e723ccc432260
Sagar3981 <sagar.pitta81@gmail.com>
2025-06-04 15:52:58 +0530

    error correction

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import express from "express";
 import userRouter from "./Routes/users.route.js";
 import cors from "cors";
-<<<<<<< HEAD
-=======
 
->>>>>>> b52a9a028dd0e64a2af9c15050b7ae3fb25280af
 const app = express();
 app.use(cors());
 app.use(express.json());
