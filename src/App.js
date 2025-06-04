b6a6d5617c82cafbfd7996e82cd62e4d4a793e35
karthikeyagullapudi <karthikgullapudi@gmail.com>
2025-06-04 17:02:40 +0530

    corrected usersignup apis

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import express from "express";
 import userRouter from "./Routes/users.route.js";
+import userSignup from "./Routes/userSignup.route.js";
 import cors from "cors";
 
 const app = express();
 app.use(express.json());
-app.use(cors);
+app.use(cors());
+
 app.use("/api", userRouter);
+app.use("/user", userSignup);
 
 export default app;
