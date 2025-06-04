7b0681e7292dad9b2b39aa0d34b631950f18a9f3
karthikeyagullapudi <karthikgullapudi@gmail.com>
2025-06-04 11:50:51 +0530

    corrected cors errors

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import express from "express";
 import userRouter from "./Routes/users.route.js";
+import cors from "cors";
+
 const app = express();
 app.use(express.json());
+app.use(cors);
 app.use("/api", userRouter);
+
 export default app;
--- src/Index.js
+++ src/Index.js
@@ -4,6 +4,6 @@ import "dotenv/config";
 
 connect().then(() => {
   app.listen(process.env.PORT, () => {
-    console.log(`port is runing on ${process.env.PORT}`);
+    console.log(`Port is runing to ${process.env.PORT}`);
   });
 });
