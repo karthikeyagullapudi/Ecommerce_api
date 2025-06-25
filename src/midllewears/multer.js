5b1baaa004e8a23807f563b8a97bc20fd7d51f37
Prabhavathi-Godatha <prabhavathigodatha15@gmail.com>
2025-06-25 11:12:24 +0530

    changes

--- src/midllewears/multer.js
+++ src/midllewears/multer.js
@@ -2,16 +2,16 @@ import multer from "multer";
 import { v4 as uuidv4 } from "uuid";
 import path from "path";
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "public");
-  },
-  filename: (req, file, cb) => {
-    const fileExtension = path.extname(file.originalname);
-    cb(null, uuidv4() + fileExtension);
-  },
-});i9olu
+    destination: (req, file, cb) => {
+        cb(null, "public");
+    },
+    filename: (req, file, cb) => {
+        const fileExtension = path.extname(file.originalname);
+        cb(null, uuidv4() + fileExtension);
+    },
+});
 
 export const upload = multer({
-  storage: storage,
-  limits: { fileSize: 10 * 1024 * 1024 },
+    storage: storage,
+    limits: { fileSize: 10 * 1024 * 1024 },
 });
