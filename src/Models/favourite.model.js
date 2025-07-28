d0a0f919f2f78295667696c320faa96f02d7b72f
Prabhavathi-Godatha <prabhavathigodatha15@gmail.com>
2025-07-28 16:49:54 +0530

    wishlist

--- src/Models/favourite.model.js
+++ src/Models/favourite.model.js
@@ -1,21 +1,21 @@
 import mongoose, { Schema } from "mongoose";
 
 const FavoriteSchema = new Schema(
-  {
-    userId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-      required: true,
+    {
+        userId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+        productId: {
+            type: String,
+            ref: "productSchema",
+            required: true,
+        },
     },
-    productId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "productSchema",
-      required: true,
-    },
-  },
-  {
-    timestamps: true,
-  }
+    {
+        timestamps: true,
+    }
 );
 
 export default mongoose.model("Favorite", FavoriteSchema);
